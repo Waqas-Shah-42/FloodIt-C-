bf254741a92a41d16631392434d921ec92e861e8
WaqasShah123 <wshah052@uottawa.ca>
2019-10-17 14:38:33 -0400

    Add project files.

---
+++ FloodIt C++/DotInfo.h
@@ -0,0 +1,14 @@
+#pragma once
+class DotInfo
+{
+	// setting up the  coordiates for te dots
+	private int x;
+	private int y;
+
+	//the colour of  the dot
+	private int colour;
+
+	//state of the dot
+	private boolean captured;
+};
+
