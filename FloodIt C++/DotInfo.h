1e553a8061f9a3bf47c878eff12b61e48407a865
WaqasShah123 <wshah052@uottawa.ca>
2019-10-17 19:05:40 -0400

    DotInfo class is complete

--- FloodIt C++/DotInfo.h
+++ FloodIt C++/DotInfo.h
@@ -1,14 +1,53 @@
 #pragma once
 class DotInfo
 {
-	// setting up the  coordiates for te dots
-	private int x;
-	private int y;
+	// setting up the  coordiates for the dots
+private:
+	int x;
+	int y;
 
 	//the colour of  the dot
-	private int colour;
+	int colour;
 
 	//state of the dot
-	private boolean captured;
+	bool captured;
+
+
+	//Contructor
+public:
+	DotInfo(int x1, int y1,int colour1) {
+		x = x1;
+		y = y1;
+		colour = colour1; 
+	}
+
+	int getX()
+	{
+		return x;
+	}
+
+	int getY()
+	{
+		return y;
+	}
+
+	void setCaptured(bool captured1)
+	{
+		captured = captured1;
+	}
+
+	bool isCaptured() 
+	{
+		return captured;
+	}
+
+	int getColour()
+	{
+		return colour;
+	}
+	void setColour(int colour1)
+	{
+		colour = colour1;
+	}
 };
 
